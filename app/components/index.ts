38fdc8631d160d546b6843e18c88708b025bc5c1
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-21 20:31:12 +0300

    Upload Form & File Uploader Component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as ResumeCard } from "./ResumeCard";
 export { default as ScoreCircle } from "./ScoreCircle";
+export { default as FileUploader } from "./FileUploader";
